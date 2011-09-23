ddc2658d80740031b31a3ffc9f044705af0d7b71
Tijs <tteulings@gmail.com>
2011-09-23 14:24:39 +0200

    added patch for mysql and negative timezones, i hope...

--- postmark/models.py
+++ postmark/models.py
@@ -101,12 +101,19 @@ def sent_message(sender, **kwargs):
         
         if not recipient[0]:
             continue
-        
-        timestamp, tz = resp["SubmittedAt"].rsplit("+", 1)
+
+        # we crazy europeans can have negative timezones
+        try:
+            timestamp, tz = resp["SubmittedAt"].rsplit("+", 1)
+        except ValueError:
+            timestamp, tz = resp["SubmittedAt"].rsplit("-", 1)
+            
         tz_offset = int(tz.split(":", 1)[0])
         tz = timezone("Etc/GMT%s%d" % ("+" if tz_offset >= 0 else "-", tz_offset))
         submitted_at = tz.localize(datetime.strptime(timestamp[:26], POSTMARK_DATETIME_STRING)).astimezone(pytz.utc)
-        
+
+        # the patch to fix MySQL issues
+        submitted_at = submitted_at.replace(tzinfo=None)
         
         emsg = EmailMessage(
             message_id=resp["MessageID"],
